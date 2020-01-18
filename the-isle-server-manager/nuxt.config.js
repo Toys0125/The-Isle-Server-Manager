0ba3290d6a94a43b4a5be11f6b31e7b518b8afb1
Toys0125 <marcus.g.stange@gmail.com>
2020-01-18 01:30:06 -0600

    Okay Realize that i need to proxy this is i want to make this into production

--- the-isle-server-manager/nuxt.config.js
+++ the-isle-server-manager/nuxt.config.js
@@ -91,6 +91,10 @@ export default {
       }
     }
   },
+  server: {
+    port: process.env.PORT, // default: 3000
+    host: process.env.HOST // default: localhost
+  },
   vue: {
     config: {
       productionTip: false,
