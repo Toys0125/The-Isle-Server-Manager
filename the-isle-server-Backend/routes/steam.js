2e9585a5e82e591e456945da115d09dd27b3f2ea
Toys0125 <marcus.g.stange@gmail.com>
2020-01-23 00:08:11 -0600

    Fixed TimeStamps for minutes place

--- the-isle-server-Backend/routes/steam.js
+++ the-isle-server-Backend/routes/steam.js
@@ -24,7 +24,8 @@ router.use(function timeLog(req, res, next) {
     date.getHours() > 9 ? date.getHours() : "0" + date.getHours();
   formattedTimer +=
     ":" +
-    date.getMinutes() +
+    date.getMinutes() > 9 ? date.getMinutes() : "0" + date.getMinutes()
+    formattedTimer +=
     ":" +
     (date.getSeconds() > 9 ? date.getSeconds() : "0" + date.getSeconds());
   console.log(formattedTimer, req.method, req.originalUrl);
