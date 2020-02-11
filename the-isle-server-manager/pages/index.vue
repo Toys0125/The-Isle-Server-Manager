c7889f7699dd0bbba6188d89b19a6c34be2ae56f
Toys0125 <marcus.g.stange@gmail.com>
2020-02-10 18:44:04 -0600

    Edit Main Page

--- the-isle-server-manager/pages/index.vue
+++ the-isle-server-manager/pages/index.vue
@@ -1,78 +1,63 @@
 <template>
-  <v-layout
-    column
-    justify-center
-    align-center
-  >
-    <v-flex
-      xs12
-      sm8
-      md6
-    >
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
       <div class="text-center">
         <logo />
         <vuetify-logo />
       </div>
       <v-card>
         <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
+          Welcome to the Isle Management Injector Tool
         </v-card-title>
         <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
+          <p></p>
           <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
+            For more information on Isle Management Injector Tool
+            <a
+              href="https://github.com/Toys0125/The-Isle-Server-Manager"
               target="_blank"
             >
-              documentation
-            </a>.
+              GitHub </a
+            >.
           </p>
           <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
+            Find a bug? Report it on the github
+            <a
+              href="https://github.com/Toys0125/The-Isle-Server-Manager/issues"
               target="_blank"
               title="contribute"
             >
-              issue board
-            </a>.
+              issue board </a
+            >.
           </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
+          <p>Thank you for visting the Isle Management Injector Tool Website</p>
           <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
+            <em><small>&mdash; @Toys0125</small></em>
           </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-          >
+          <hr class="my-3" />
+          <a href="https://nuxtjs.org/" target="_blank">
             Nuxt Documentation
           </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-          >
+          <br />
+          <a href="https://github.com/nuxt/nuxt.js" target="_blank">
             Nuxt GitHub
           </a>
         </v-card-text>
         <v-card-actions>
           <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
+          <dir v-if="!isUserLoggedIn()">
+            <v-btn color="primary" nuxt to="/login">
+              Login
+            </v-btn>
+          </dir>
+          <dir v-else>
+            <v-btn color="primary" nuxt to="/edit">Edit Players </v-btn>
+            <dir v-if="isAdminAccount()">
+              <v-btn color="primary" nuxt to="/users"
+                >Edit Management Accounts
+              </v-btn>
+            </dir>
+          </dir>
         </v-card-actions>
       </v-card>
     </v-flex>
@@ -80,13 +65,27 @@
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
+import Logo from "~/components/Logo.vue";
+import VuetifyLogo from "~/components/VuetifyLogo.vue";
 
 export default {
   components: {
     Logo,
     VuetifyLogo
+  },
+  methods: {
+    isUserLoggedIn() {
+      //console.log("Is the user logged in",this.$auth.loggedIn)
+      return this.$auth.loggedIn;
+    },
+    isAdminAccount() {
+      if (
+        this.$auth.$storage.getUniversal("scope").includes("Admin") ||
+        this.$auth.$storage.getUniversal("scope").includes("Master")
+      ) {
+        return true;
+      } else return false;
+    }
   }
-}
+};
 </script>
