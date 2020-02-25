ee23d91bb62ed325a51e6a039d33de688c6eb97d
Toys0125 <marcus.g.stange@gmail.com>
2020-02-24 23:24:29 -0600

    More testing of key

--- the-isle-server-Backend/index.js
+++ the-isle-server-Backend/index.js
@@ -14,6 +14,7 @@ var credentials = {
     cert:cert
 }
 console.log(key,cert,"Key or Cert Are true or null")
+console.log(fs.readdirSync(path.resolve(process.cwd(),'../')).filter(item =>item.endsWith('.key')))
 var https = key&&cert?require('https'):null
 var steamRouter = require('./routes/steam')
 var loginRouter = require('./routes/login')
